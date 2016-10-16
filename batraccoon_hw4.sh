9fd0898393b136c78921c9b65fee23c4b0a14433
Alex <alexcragun@weber.edu>
2016-10-16 16:24:27 -0600

    ftp works

--- batraccoon_hw4.sh
+++ batraccoon_hw4.sh
@@ -44,13 +44,23 @@ tar -vxzf MOCK_DATA_$year.tar.gz
 
 for files in "MOCK_DATA*.csv"
 do
+	sed -e 's/,,/,waldo@weber.edu,/g' temp.txt > temp.txt
 	awk -F"," '$5==Female && $6==Canada {print $2" "$3" "$4}' $files > temp.txt
-	if (-z $4)
-	then 
-		sed -e 's/,,/,waldo@weber.edu,/g' temp.txt > temp.txt
-	fi
 done
 
 #compress using zip
+datetime=`date +%Y_%m_%d_%H:%M` 
+zip MOCK_DATA_FILTER_$datetime.zip temp.txt
 
+HOST=137.190.19.87
+USER=$USER
+PASSWORD=$psswrd
+ftp -inv $HOST <<EOF
+user $USER $PASSWORD
+cd
+bye
+EOF
+
+cd ..
+rm -r temp
 exit 0
