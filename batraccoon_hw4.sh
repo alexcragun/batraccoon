6a802cd0ca86917a1f84920918d973ae34d64dc5
JeremyMarcusen <jm78227@icarus.cs.weber.edu>
2016-10-16 23:50:43 +0000

    reuploading after finishing 5 and implementing basic fixes

--- batraccoon_hw4.sh
+++ batraccoon_hw4.sh
@@ -22,45 +22,58 @@ usage="usage: [-y year] [-e email] [-u user] [-p password]"
 while getopts ":y:e:u:p" opt
 do
 	case $opt in
-		y)year=$OPTARG;;
-			if ( ! = $year )
-				echo "please put in year"
-				exit 1
-			fi
-		e)email=$OPTARG;;
-			if ( ! = $email)
-				email="waldo@weber.edu"
-			fi;;
-		u)user=$OPTARG;;
-		p)psswrd=$OPTARG
+		y) year=$OPTARG;;
+		e) email=$OPTARG;;
+		u) user=$OPTARG;;
+		p) psswrd=$OPTARG;;
 	esac
 done
 
+if [[ -z $year ]]
+then
+	echo "please enter year"
+	exit 1
+fi
+
+if [[ -z $email ]]
+then
+	set $email="waldo@weber.edu"
+fi
+
 #Make temp file and decompress
-mkdir temp
-cd temp
-wget icarus.cs.weber.edu/~hvalle/cs3030/MOCK_DATA_$year.tar.gz
-tar -vxzf MOCK_DATA_$year.tar.gz
+echo "Aquiring data"
+`mkdir temp`
+`cd temp`
+`wget icarus.cs.weber.edu/~hvalle/cs3030/MOCK_DATA_$year.tar.gz`
+`tar -vxzf MOCK_DATA_$year.tar.gz`
+
+echo "Filtering Data"
 
 for files in "MOCK_DATA*.csv"
 do
-	sed -e 's/,,/,waldo@weber.edu,/g' temp.txt > temp.txt
-	awk -F"," '$5==Female && $6==Canada {print $2" "$3" "$4}' $files > temp.txt
+	`sed -e 's/,,/,waldo@weber.edu,/g' temp.txt > temp.txt`
+	`awk -F"," '$5==Female && $6==Canada {print $2" "$3" "$4}' $files > temp.txt`
 done
+
+echo "Data Filtered"
 
 #compress using zip
 datetime=`date +%Y_%m_%d_%H:%M` 
-zip MOCK_DATA_FILTER_$datetime.zip temp.txt
+`zip MOCK_DATA_FILTER_$datetime.zip temp.txt`
 
 HOST=137.190.19.87
 USER=$USER
 PASSWORD=$psswrd
-ftp -inv $HOST <<EOF
+
+`ftp -inv $HOST << EOF`
 user $USER $PASSWORD
 cd
 bye
 EOF
 
-cd ..
-rm -r temp
+`cd ..`
+`rm -r temp`
+
+echo "Data deposited"
+
 exit 0
